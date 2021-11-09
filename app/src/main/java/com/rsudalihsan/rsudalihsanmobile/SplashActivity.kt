b32b7cdc855316250f1cb311cc007665bb781ade
Dikdik Firman Sidik <dikdikalihsan@gmail.com>
2021-11-09 07:52:17 +0700

    edit manifest untuk tampilan pertama

--- app/src/main/java/com/rsudalihsan/rsudalihsanmobile/SplashActivity.kt
+++ app/src/main/java/com/rsudalihsan/rsudalihsanmobile/SplashActivity.kt
@@ -2,11 +2,14 @@ package com.rsudalihsan.mobilersudalihsan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.rsudalihsan.rsudalihsanmobile.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        
     }
 }
